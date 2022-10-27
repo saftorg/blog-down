a392cf6aaa98aa1df6014346143db9d170186f01
Steven John <stevenjpx2@gmail.com>
2022-10-27 18:09:00 +0530

    feat: created website

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div
+    class="grid bg-slate-50 dark:bg-slate-800 w-screen h-screen place-items-center px-5"
+  >
+    <div
+      class="max-w-xl bg-slate-200 dark:bg-slate-900 rounded-lg h-fit p-10 text-center"
+    >
+      <img
+        src="~/assets/logo Medium-2.webp"
+        class="block dark:hidden w-52 mx-auto mb-7"
+      />
+      <img
+        src="~/assets/logo-white.webp"
+        class="hidden dark:block w-52 mx-auto mb-7"
+      />
+      <h1 class="text-3xl font-bold mb-3 text-slate-900 dark:text-slate-50">
+        We'll be right back.
+      </h1>
+      <p class="text-slate-900 text-lg dark:text-slate-50">
+        We seem to be facing some issues right now, but we're working overtime
+        to solve and bring you the best experience possible!
+      </p>
+    </div>
   </div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap");
+
+html {
+  font-family: "Manrope", sans-serif;
+}
+</style>
